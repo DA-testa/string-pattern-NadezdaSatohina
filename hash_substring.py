c838496c4170f0275f82f53dd5eceee605ecdc46
NadezdaSatohina <124875434+NadezdaSatohina@users.noreply.github.com>
2023-04-11 15:41:14 +0000

    test

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,48 @@
 # python3
 
-def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+def hash(s, sz):
+    h = 0
+    for i in range(0, sz):
+        h <<= 8
+        h += ord(s[i])
+       
+    return h
+
+def search(s, sz, nh):
+    hh = hash(s, sz)
+    mask = 0x100 ** (sz - 1) - 1
+    for i in range(0, len(s)):
+        if nh == hh:
+            yield i
+
+        hh &= mask
+        hh <<= 8
+       
+        if i + sz < len(s):
+            hh += ord(s[i + sz])
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
-
-    # and return an iterable variable
-    return [0]
-
+    return list(search(text, len(pattern), hash(pattern, len(pattern))))
 
 # this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    input_type = input("Input Type: ")
+    if "F" in input_type:
+        filename = input("Input File Name: ")
+
+        if "test/" not in filename:
+            filename = "test/" + filename
+        if "test/" in filename:    
+            with open(filename) as f:
+                pattern = f.readline().strip()
+                text = f.readline().strip()
+    elif "I" in input_type:
+        pattern = input("Pattern: ").strip()
+        text = input("Text: ").strip()
+
+    print_occurrences(get_occurrences(pattern, text))
 
